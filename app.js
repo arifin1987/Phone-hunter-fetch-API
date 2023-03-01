f7fe6982bc0c52b7a329a83f30a03334e6baba7e
Shohidul Arifin <shohidularifin@gmail.com>
2023-03-01 06:14:04 -0800

    show details information shown as a modal

--- app.js
+++ app.js
@@ -47,7 +47,9 @@ const displayPhones = (phones,dataLimit) =>{
                 <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">show details</button>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">show details</button>
+                
+                
                 </div>
                 </div>
                 
@@ -101,6 +103,17 @@ const loadPhoneDetails =async id=>{
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    displayPhoneDetails(data.data);
 
+}
+
+const displayPhoneDetails= phone =>{
+const modalTitle = document.getElementById('phoneDetailModalLabel');
+modalTitle.innerText = phone.name;
+const phoneDetails = document.getElementById('phone-details');
+phoneDetails.innerHTML =`
+<p>Release Date: ${phone.releaseDate ? phone.releaseDate :'No release date found'}</p>
+<p>Others: ${phone.others ? phone.others.Bluetooth: 'No bluetooth information'}</p>
+
+`
 }
